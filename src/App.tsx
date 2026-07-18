749fed2fa88c949a736d6dd75d58ecee640454ff
PerformancePrestigeAcademy <performanceprestigeacademy@gmail.com>
2026-07-18 23:11:41 +0200

    PATCH 009 v2

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import FormationPage from "./Pages/FormationPage";
 import MaintenancePage from "./Pages/MaintenancePage";
+import MentionsLegales from "./pages/MentionsLegales";
+import PolitiqueConfidentialite from "./pages/PolitiqueConfidentialite";
 
 export default function App() {
   return (
@@ -15,6 +17,10 @@ export default function App() {
         <Route path="/formation" element={<FormationPage />} />
 
         <Route path="/maintenance" element={<MaintenancePage />} />
+
+        <Route path="/mentions-legales" element={<MentionsLegales />} />
+
+        <Route path="/politique-confidentialite" element={<PolitiqueConfidentialite />} />
 
       </Routes>
 
