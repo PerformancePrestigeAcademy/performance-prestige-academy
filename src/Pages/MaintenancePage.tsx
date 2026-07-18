e72d5066d09c5fa65f76a29614902f836c1eb778
PerformancePrestigeAcademy <performanceprestigeacademy@gmail.com>
2026-07-18 23:52:44 +0200

    PATCH 010 v2

--- src/Pages/MaintenancePage.tsx
+++ src/Pages/MaintenancePage.tsx
@@ -28,7 +28,7 @@ export default function MaintenancePage() {
 
   <div className="max-w-7xl mx-auto px-6">
 
-    <div className="grid md:grid-cols-2 lg:grid-cols-5 gap-6">
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-8 max-w-6xl mx-auto">
 
       <a
         href="#entretien"
