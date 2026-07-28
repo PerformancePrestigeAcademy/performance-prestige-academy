7d544e4d07ae92ccad0dd0c88daf1e352c363548
PerformancePrestigeAcademy <performanceprestigeacademy@gmail.com>
2026-07-28 20:03:11 +0200

    PATCH 018 v12
    navbar

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -2,6 +2,7 @@ export const NAV_LINKS = [
   { label: "Accueil", href: "/" },
   { label: "Maintenance", href: "/maintenance" },
   { label: "Formation", href: "/formation" },
+  { label: "Préparation & Développement", href: "/preparation-developpement"  },
   { label: "À propos", href: "/apropos" },
   { label: "Contact", href: "/contact" },
 ];
