635d9a994456640039cabac2a2ea1de8c5446e5e
PerformancePrestigeAcademy <performanceprestigeacademy@gmail.com>
2026-09-11 16:34:12 +0200

    PATCH 028 v11

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,8 +1,8 @@
 export const NAV_LINKS = [
   { label: "Accueil", href: "/" },
-  { label: "Maintenance", href: "/maintenance" },
   { label: "Formation", href: "/formation" },
   { label: "Préparation & Développement", href: "/preparation-developpement"  },
+  { label: "Maintenance", href: "/maintenance" },
   { label: "À propos", href: "/a-propos" },
   { label: "Contact", href: "/contact" },
 ];
